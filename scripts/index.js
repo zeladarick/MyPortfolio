9913bc8373af4dc5afb438a46d3591e06635d387
Ricardo Zelada <zeladarick@gmail.com>
2020-10-22 21:14:42 -0700

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+  function twitter(){
+    window.open("https://twitter.com/zeladarick", "blank_");
+  }
+  function instagram(){
+    window.open("https://www.instagram.com/zeladarick", "blank_");
+  }
+  function linkedin(){
+    window.open("https://www.linkedin.com/in/ricardo-zelada-223220171/", "blank_");
+  }
+
+  function irA(){
+    document.body.style.cursor = "pointer";
+  }
+
+  function normal(){
+    document.body.style.cursor = "default";
+  }
